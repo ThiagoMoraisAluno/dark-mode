53710e81fa687e5119dad62c5a5f0a83112b4f1a
Thiago Morais Cordeiro <thiagomoraiscordeiro@outlook.com>
2023-02-04 11:30:48 -0300

    criando as funções para o salvamento de preferência do usuário, commit final

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,30 @@
 const changeThemeBtn = document.querySelector("#change-theme");
 
-changeThemeBtn.addEventListener("change", function(){
+//Toggle dark mode
+function toggleDarkmode(){
     document.body.classList.toggle("dark");
+}
+
+//carregando o light e dark mode
+
+function loadTheme(){
+    const darkMode = localStorage.getItem("dark");
+
+    if (darkMode){
+        toggleDarkmode();
+    }
+}
+
+loadTheme()
+
+changeThemeBtn.addEventListener("change", function(){
+    toggleDarkmode();
+
+    //salvar ou remover-esta função que irá definir a preferência do usuário
+
+    localStorage.removeItem("dark");
+
+    if (document.body.classList.contains("dark")){
+        localStorage.setItem("dark", 1);
+    }
 });
